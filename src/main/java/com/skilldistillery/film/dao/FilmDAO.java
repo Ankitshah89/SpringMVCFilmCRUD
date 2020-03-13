08274894783d6b6a9a230aca1c8e5d77dd0cbad9
Ankitshah89 <shahdankit@gmail.com>
2020-03-13 16:55:05 -0600

    Added folders

---
+++ src/main/java/com/skilldistillery/film/dao/FilmDAO.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.film.dao;
+
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+  public Film findFilmById(int filmId);
+  public Actor findActorById(int actorId);
+  public List<Actor> findActorsByFilmId(int filmId);
+  public List<Film> findFilmByActorId(int actorId);
+  public List<Film> findFilmByKeywords(String keyword);
+  public String getLanguageOfFilm(int filmId);
+}
